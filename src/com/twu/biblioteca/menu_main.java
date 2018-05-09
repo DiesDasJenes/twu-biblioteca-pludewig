7cab6f1d671cb703d8e107a6bd46c2df4bb5aa4e
alaska <mail@philippludewig.de>
2018-05-09 15:59:17 +0200

    Added Welcome Message

---
+++ src/com/twu/biblioteca/menu_main.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+class menu_main {
+
+    static void printWelcomeMessage(){
+        System.out.println("##########################################");
+        System.out.println("#\t\t\t\tBiblioteca\t\t\t\t#");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("#\t\tNihil Sciemus Sed Est Exiguus\t#");
+        System.out.println("##########################################");
+        System.out.println("\t\t\t   We welcome you");
+    }
+}
---
+++ test/com/twu/biblioteca/menu_mainTest.java
@@ -0,0 +1,20 @@
+package com.twu.biblioteca;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import static org.junit.Assert.*;
+
+public class menu_mainTest {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Test
+    public void printWelcomeMessage() {
+        menu_main.printWelcomeMessage();
+
+        assertEquals("##########################################\n#\t\t\t\tBiblioteca\t\t\t\t#\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n#\t\tNihil Sciemus Sed Est Exiguus\t#\n##########################################\n\t\t\t   We welcome you",systemOutRule.getLog().substring(0,205));
+    }
+}
