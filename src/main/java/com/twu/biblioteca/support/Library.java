1790f49f9041f814d92d244cba50ad6ee660791a
alaska <mail@philippludewig.de>
2018-05-15 15:09:40 +0200

    clean up project structure

--- src/main/java/com/twu/biblioteca/support/Library.java
+++ src/main/java/com/twu/biblioteca/support/Library.java
@@ -1,16 +1,14 @@
 package com.twu.biblioteca.support;
 
 
-import io.bloco.faker.Faker;
-import org.joda.time.LocalDate;
-
-import java.util.HashMap;
 import java.util.Map;
-public class Library  {
+
+public class Library {
 
     private static final int AMOUNT_OF_BOOKS = 25;
     private Map<String, Book> ListOfAllBooks;
     private MockModel_Customer MockModel_Customer;
+
     public Library() {
         FakeDataGenerator FDG = new FakeDataGenerator();
         ListOfAllBooks = FDG.initiateBookList(AMOUNT_OF_BOOKS);
@@ -24,25 +22,16 @@ public class Library  {
     }
 
 
-
-    public void addBooks(Book... books){
-        for (Book b:books
-             ) {
-            ListOfAllBooks.putIfAbsent(b.getBookId(),b);
+    public void addBooks(Book... books) {
+        for (Book b : books
+                ) {
+            ListOfAllBooks.putIfAbsent(b.getBookId(), b);
         }
     }
-
 
 
     public Map<String, Book> getListOfAllBooks() {
         return ListOfAllBooks;
     }
 
-    public void setBook(String key, Book Book) {
-        ListOfAllBooks.replace(key,Book);
-    }
-
-    public void setCustomer(int key, Customer Customer) {
-        MockModel_Customer.getListofCustomers().replace(key,Customer);
-    }
 }
--- src/main/java/com/twu/biblioteca/support/Messages.java
+++ src/main/java/com/twu/biblioteca/support/Messages.java
@@ -1,23 +1,21 @@
 package com.twu.biblioteca.support;
 
-import java.io.IOException;
-
 public class Messages {
 
     private static final int MAXLENGTHLINE = 50;
 
     public static void printWelcomeMessage() {
         printCharacterLine('#');
-        printStringinMiddleSorroundedWith('#',"Biblioteca");
+        printStringinMiddleSorroundedWith('#', "Biblioteca");
         printCharacterLine('~');
-        printStringinMiddleSorroundedWith('#',"Nihil Sciemus Sed Est Exiguus");
+        printStringinMiddleSorroundedWith('#', "Nihil Sciemus Sed Est Exiguus");
         printCharacterLine('#');
         printStringinMiddleSorroundedWith('#', "May you be greeted");
         printCharacterLine('#');
         System.out.print("\n");
     }
 
-    static void printCharacterLine(char c){
+    static void printCharacterLine(char c) {
         for (int i = 0; i < MAXLENGTHLINE; i++) {
             System.out.print(c);
         }
@@ -25,13 +23,13 @@ public class Messages {
     }
 
 
-    static void printStringinMiddleSorroundedWith(char c,String something){
+    static void printStringinMiddleSorroundedWith(char c, String something) {
         System.out.print(c);
-        for (int i = 0; i < MAXLENGTHLINE-2; i++) {
+        for (int i = 0; i < MAXLENGTHLINE - 2; i++) {
             System.out.print(" ");
-            if(i == ((MAXLENGTHLINE/2)-(something.length()/2))){
+            if (i == ((MAXLENGTHLINE / 2) - (something.length() / 2))) {
                 System.out.print(something);
-                i+=something.length();
+                i += something.length();
             }
         }
         System.out.print(c);
